23496e1df9655eb1314e17076d634ee6e1923162
Sergey Malyarov <sergey@malyaroff.com>
2023-06-06 22:06:35 +0300

    Unitest commit

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -2,6 +2,35 @@ import unittest
 from utils import arrs
 
 
+
+class TestArrs(unittest.TestCase):
+
+    def test_get(self):
+        self.assertEqual(get([1, 2, 3], 1, "test"), 2)
+        self.assertEqual(get([], 0, "test"), "test")
+
+    def test_my_slice(self):
+        # Тест для нормальной ситуации со срезом от одного элемента до другого
+        self.assertEqual(my_slice([1, 2, 3, 4], 1, 3), [2, 3])
+
+        # Тест для ситуации со срезом всего массива
+        self.assertEqual(my_slice([1, 2, 3, 4]), [1, 2, 3, 4])
+
+        # Тест для ситуации, когда начало среза больше длины массива
+        self.assertEqual(my_slice([1, 2, 3, 4], 5), [])
+
+        # Тест для ситуации, когда начало среза отрицательное
+        self.assertEqual(my_slice([1, 2, 3, 4], -2), [3, 4])
+
+        # Тест для ситуации, когда начало и конец среза отрицательные
+        self.assertEqual(my_slice([1, 2, 3, 4], -3, -1), [2, 3])
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
+
 class TestArrs(unittest.TestCase):
 
     def test_get(self):
